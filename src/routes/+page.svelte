f69de1fb1fee7775195183b3196b54a8b2ba0cc1
AnotherPillow <85362273+AnotherPillow@users.noreply.github.com>
2024-05-15 18:41:46 +0100

    Add custom fixers

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,144 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import type { ChangeEventHandler } from "svelte/elements";
+    import readFileContent from "$lib/readFileContent";
+    import { page } from '$app/stores';
+    import { get } from "svelte/store";
+
+    let createMode = false
+
+    let saveFiles: Array<File> = [] as any;
+    let fileContent: string = ''
+
+    let saveFileUploadError: string = '';
+    let saveFileDownloadLink: string = '';
+
+    const params = new URLSearchParams(get(page).url.search);
+
+    let beforeRe = params.get('before') ?? ''
+    let afterRe = params.get('after') ?? ''
+    let fixText = params.get('title') ?? ''
+
+
+    
+    if (!beforeRe || !afterRe || !fixText) createMode = true
+    if (params.has('creation')) createMode = true
+
+    const handleUploadedFile: ChangeEventHandler<HTMLInputElement> = async (e) => {
+        const { files } = e.currentTarget
+        if (files?.length == 0 || !files) return false
+        
+        saveFiles = files as unknown as File[]
+        const file = files[0]
+
+        if (
+               file.name.includes('.') 
+            || file.size < 1e5 /*100,000*/ 
+        ) {
+            saveFileUploadError = `${file.name} is not a valid Stardew Valley save file!`
+            // return false;
+            return;
+        } else {
+            saveFileUploadError = ''
+        }
+
+        console.log(file)
+        const content = await readFileContent(file)
+        const preRegex = new RegExp(beforeRe, 'gm')
+
+        console.log(preRegex, afterRe)
+
+        const newContent = content.replace(
+            preRegex,
+            afterRe
+        )
+
+        const newFile = new File(newContent.split('\n'), file.name, {
+            type: file.type,
+            lastModified: file.lastModified
+        })
+
+        saveFileDownloadLink = URL.createObjectURL(newFile)
+        
+        
+    }
+</script>
+
+
+{#if !createMode}
+    <h1>{fixText}</h1>
+    <h3>Upload your save file to run this fixer/modifier.</h3>
+
+    <h4 class="third-party-warning">This is created by a third-party and stardew.rocks is not responsible for any issues that may occur. Always back up your save before using a save editor.</h4>
+    
+    <br />
+    <label for="save-upload-input">Upload your save file (Name_123456789)</label>
+    <input 
+        id="save-upload-input"
+        type="file"
+        on:change={handleUploadedFile}
+    />
+    {#if saveFileUploadError && saveFileUploadError != ''}
+        <h3 class="error">{saveFileUploadError}</h3>
+    {:else if saveFileDownloadLink}
+        <a href={saveFileDownloadLink} download>Download modified save</a>
+    {/if}
+
+    <details>
+        <summary>See replacing text</summary>
+        <span><code>/{beforeRe}/gm</code> -> <code>{afterRe}</code></span>
+    </details>
+{:else}
+    <h1>Create your own save fixer/modifier</h1>
+
+    <label for="fix-title">Fix title</label>
+    <input id="fix-title" type="text" bind:value={fixText}>
+    <br />
+    
+    <label for="before-regex">Match Regex (Javascript, <code>/xyz/gm</code>)</label>
+    <input id="before-regex" type="text" bind:value={beforeRe}>
+    <br />
+    
+    <label for="after-regex">Match Replacement</label>
+    <input id="after-regex" type="text" bind:value={afterRe}>
+    <br />
+    <br />
+
+    <button id="create-fix-btn" on:click={() => {
+        const linkWithoutParams = window.location.href.replace(window.location.search, '')
+        window.location.href = linkWithoutParams + `?before=${encodeURIComponent(beforeRe)}&after=${encodeURIComponent(afterRe)}&title=${fixText}`
+    }}>Create!</button>
+{/if}
+
+<style lang="scss">
+
+    .error {
+        color: red;
+        font-weight: bold;
+    }
+
+    .third-party-warning {
+        color: red;
+
+        font-weight: bold;
+        
+        border-radius: 2px;
+    }
+
+    #create-fix-btn {
+        outline: none;
+        
+        border: 2px solid rgb(0, 74, 0);
+        border-radius: 5px;
+        
+        background-color:  green;
+        color: white;
+        
+        width: 5em;
+        height: 2em;
+    }
+
+    details {
+        position: absolute;
+        bottom: 10%;
+    }
+</style>
